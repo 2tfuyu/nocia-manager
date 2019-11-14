6396359a1b9197b99e9afdd12396ba7ce2dd48ca
github nitf <2tf2003@gmail.com>
2019-11-14 10:28:48 +0900

    add: News Model

---
+++ nocia-manager/nocia-manager/News.cs
@@ -0,0 +1,15 @@
+﻿namespace nocia_manager
+{
+    internal class News
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+        public string date { get; set; }
+        public string body { get; set; }
+
+        public override string ToString()
+        {
+            return $"{title}";
+        }
+    }
+}
